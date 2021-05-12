e834d54196958579de155dbd2a4afe1be0dd8ceb
gustavocpo <gustavocpo@gmail.com>
2021-05-12 14:01:18 -0300

    commit

--- plotagem.py
+++ plotagem.py
@@ -1,16 +1,18 @@
-Planetas = {"terra": {"nome": "terra", "tamanho": 3, "US": 2, "dist_x": 2, "dist_y": 2},
-            "marte": {"nome": "marte", "tamanho": 2, "US": 20, "dist_x": 1, "dist_y": 3},
-            "vesculi": {"nome": "vesculi", "tamanho": 2, "US": 3, "dist_x": 2, "dist_y": 6}
+Planetas = {"terra": {"nome": "terrai ", "tamanho": 3, "US": 2, "dist_x": 2, "dist_y": 2},
+            "marte": {"nome": "martei ", "tamanho": 2, "US": 20, "dist_x": 1, "dist_y": 3},
+            "vesculi": {"nome": "vescul ", "tamanho": 2, "US": 3, "dist_x": 4, "dist_y": 6}
             }
 achou = False
-for l in range(1, 7):
-    for c in range(1, 7):
+for l in range(1, 16):
+    for c in range(1, 16):
         for k in Planetas:
             if l == Planetas[k]["dist_x"] and c == Planetas[k]["dist_y"]:
                 achou = True
+                npla = Planetas[k]["nome"]
         if achou is False:
-            print("# ", end='')
+            print("# "*4, end='')
         else:
-            print(". ", end='')
+            print(npla + " ", end='')
+            #print(". "*4, end='')
             achou = False
     print("")
