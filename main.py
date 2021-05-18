b6ea7c3eeff2f5edbc7c74b3e306f0abea398891
gustavocpo <gustavocpo@gmail.com>
2021-05-18 14:32:52 -0300

    commit

--- main.py
+++ main.py
@@ -236,7 +236,22 @@ Goods = {
     "alcool": 5,
     "tiberium": 7
 }
-
+Planetas = {"tudush": {"nome": "tudush", "tamanho": 1, "US": 1, "dist_x": 13, "dist_y": 9},
+            "momouc": {"nome": "momouc", "tamanho": 2, "US": 2, "dist_x": 2,  "dist_y": 8},
+            "zapis":  {"nome": "zapis", "tamanho": 3, "US": 1, "dist_x": 12, "dist_y": 7},
+            "chukna": {"nome": "chukna", "tamanho": 5, "US": 2, "dist_x": 9,  "dist_y": 14},
+            "kkanik": {"nome": "kkanik", "tamanho": 3, "US": 2, "dist_x": 8,  "dist_y": 6},
+            "apicat": {"nome": "apicat", "tamanho": 2, "US": 1, "dist_x": 5,  "dist_y": 1},
+            "hstula": {"nome": "hstula", "tamanho": 1, "US": 2, "dist_x": 11, "dist_y": 4},
+            "piktle": {"nome": "piktle", "tamanho": 3, "US": 1, "dist_x": 3,  "dist_y": 5},
+            "rupita": {"nome": "rupita", "tamanho": 3, "US": 2, "dist_x": 15, "dist_y": 11},
+            "bosshi": {"nome": "bosshi", "tamanho": 4, "US": 1, "dist_x": 10, "dist_y": 3},
+            "karaii": {"nome": "karaii", "tamanho": 3, "US": 2, "dist_x": 7,  "dist_y": 13},
+            "teknot": {"nome": "teknot", "tamanho": 3, "US": 1, "dist_x": 14, "dist_y": 2},
+            "terra":  {"nome": "terra", "tamanho": 3, "US": 2, "dist_x": 6,  "dist_y": 12},
+            "marte":  {"nome": "marte", "tamanho": 2, "US": 5, "dist_x": 1,  "dist_y": 15},
+            "vescul": {"nome": "vescul", "tamanho": 2, "US": 3, "dist_x": 4,  "dist_y": 10}
+            }
 #  FIM DICIONARIO  #
 
 if __name__ == "__main__":
